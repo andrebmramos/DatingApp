0d9caa8ca02cb6520e55cba09e0a33650e09f21f
André Ramos <andrebmramos@gmail.com>
2019-06-16 22:48:15 -0300

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    }
+
+}
