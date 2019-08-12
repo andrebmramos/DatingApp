5dea9571f7cb35bd6472928deed8c948d17370f8
André Ramos <andrebmramos@gmail.com>
2019-08-12 00:25:25 -0300

    End of section 6

---
+++ DatingApp-SPA/src/app/_services/alertify.service.ts
@@ -0,0 +1,48 @@
+// Esse serviço faz uma série de wrappers para as funcçoes do alertifyjs, de modo
+// poder usá-las elegantemente em todo programa simplemente injetando serviço
+
+
+import { Injectable } from '@angular/core';
+import { success } from 'node_modules/alertifyjs/build/alertify.min.js';
+declare let alertify: any; // Para evitar ficar mostrando erros, uma vez que
+                           // Alertify está importado nos services do arquivo angular.json
+                           // e para não ter que importar cada função, como fiz acima com success
+                           // só para exemplificar
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertifyService {
+
+
+constructor() { }
+
+
+confirm(message: string, okCallBack: () => any) {
+  alertify.confirm(message, function(e) {
+    if (e) { // onde 'e' será o evento de clicar no ok, e okCallBack é o que acontecerá depois
+      okCallBack();
+    } else {} // nada a fazer se clicar cancel
+  });
+}
+
+success(message: string) {
+  // alertify.success(message);
+  success(message);
+}
+
+error(message: string) {
+  alertify.error(message);
+}
+
+warning(message: string) {
+  alertify.warning(message);
+}
+
+message(message: string) {
+  alertify.message(message);
+}
+
+
+
+}
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,7 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};  // <==
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -17,23 +18,35 @@ export class NavComponent implements OnInit {
   login() {
     console.log(this.model);
     this.authService.login(this.model).subscribe( next => {
-      console.log('Logged in successfully');
+      // console.log('Logged in successfully');
+      this.alertify.success('Logged in successfully');
     }, error => {
       // console.log('Failed to login');
-      console.log(error);
+      // console.log(error);
+      this.alertify.error(error);
     });
   }
 
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token; // se há algo no token, operador !! retorna true, ou seja, há token ativo, usuário está logado
+    // Primeira forma que fizermos, apenas verifica se tem algo em localstorage (lá estávamos guardando o token)
+    // sem fazer validação (nem confirmar se é um token de fato)
+    // Não era problema de segurança, mas certamente era tosco. Vamos passar a administra o Token por meio de
+    // ferramena específica: auth0/angular2-jwt -- npm install @auth0/angular-jwt
+    // const token = localStorage.getItem('token');
+    // return !!token; // se há algo no token, operador !! retorna true, ou seja, há token ativo, usuário está logado
+
+    // colocamos o JwtHelperService no nosso AuthService. Lá o token será verificado. Chamamos aquela função daqui de dentro
+    // pra não ter que mexer no código
+    return this.authService.loggedIn();
+
   }
 
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    // console.log('logged out');
+    this.alertify.message('logged out');
   }
 
 }
